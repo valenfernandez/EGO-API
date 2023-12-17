a5bbed8232ad22d8be982a208181df538bcd3e6f
valenfernandez <valen.fernandez.montenegro@gmail.com>
2023-12-17 17:44:23 -0300

    add: models registration

--- EGO Cars/ego/cars_api/admin.py
+++ EGO Cars/ego/cars_api/admin.py
@@ -3,3 +3,11 @@ from .models import Vehicle, Feature, Review, Concessionaire, TestDrivePetition,
 
 # Register your models here.
 
+admin.site.register(Vehicle)
+admin.site.register(Feature)
+admin.site.register(Review)
+admin.site.register(Concessionaire)
+admin.site.register(TestDrivePetition)
+admin.site.register(Service)
+admin.site.register(Accesory)
+admin.site.register(Activity)
